8d7feb4feb1e4ec769c826ae1b7aec222b3f8dbb
Giuli Ilustre <ilustre.giuli@gmail.com>
2021-01-15 14:41:41 -0300

    class position

---
+++ application/Program.java
@@ -0,0 +1,14 @@
+package secao16_xadrez.application;
+
+import secao16_xadrez.boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position pos = new Position(3,5);
+		System.out.println(pos);
+
+	}
+
+}
---
+++ boardgame/Position.java
@@ -0,0 +1,34 @@
+package secao16_xadrez.boardgame;
+
+public class Position {
+	
+	private int row;
+	private int column;
+	
+	public Position() {
+	}
+
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return this.row + ", " + this.column;
+	}
+	
+}
